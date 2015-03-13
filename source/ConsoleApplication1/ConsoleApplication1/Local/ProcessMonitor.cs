79087c5f5eaab7c7448fb61a085094df224b30b6
smallproject <j.a.villas@live.com.ph>
2015-03-13 16:39:32 +0100

    removed unused library

--- source/ConsoleApplication1/ConsoleApplication1/Local/ProcessMonitor.cs
+++ source/ConsoleApplication1/ConsoleApplication1/Local/ProcessMonitor.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication1.Local
 {
